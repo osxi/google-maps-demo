e08fdb55b79afba891ee8421c5bbaf4bb2eae291
osxi <osxi@users.noreply.github.com>
2015-02-24 10:32:48 -0600

    index on (no branch): f87e140 WIP on master: d955451 Factor out createMarker

--- places_client.js
+++ places_client.js
@@ -1,19 +1,18 @@
 var map,
-    infowindow,
+    infoWindow,
     currentType,
-    markersArray = [];
+    keyWordOverride,
+    service;
 
-function initialize_map(el, lat, lon, locationMarkerText, showType) {
+function initialize_map(el, lat, lon, locationMarkerText, showType, keyWord) {
   var location = new google.maps.LatLng(lat, lon);
 
-  if (showType !== undefined) {
-    currentType = showType[0];
-  }
-  // console.log('currentType', currentType);
+  currentType = showType;
+  keyWordOverride = keyWord;
 
   map = new google.maps.Map(document.getElementById(el), {
     center: location,
-    zoom: 14
+    zoom: 12
   });
 
   var locationMarker = new google.maps.Marker({
@@ -22,86 +21,59 @@ function initialize_map(el, lat, lon, locationMarkerText, showType) {
   });
 
   google.maps.event.addListener(locationMarker, 'click', function() {
-    infowindow.setContent(locationMarkerText);
-    infowindow.open(map, this);
+    infoWindow.setContent(locationMarkerText);
+    infoWindow.open(map, this);
   });
 
-  var request = {
-    location: location,
-    radius: 10000,
-    types: showType
-  };
+  infoWindow = new google.maps.InfoWindow();
+  service = new google.maps.places.PlacesService(map);
+  google.maps.event.addListenerOnce(map, 'bounds_changed', performSearch);
+}
 
-  infowindow = new google.maps.InfoWindow();
-  var service = new google.maps.places.PlacesService(map);
-  service.nearbySearch(request, callback);
+function performSearch() {
+  var request = {
+    bounds: map.getBounds(),
+    type: currentType,
+    keyword: keyWordOverride || currentType
+  };
+  service.radarSearch(request, callback);
 }
 
 function callback(results, status, pagination) {
   if (status != google.maps.places.PlacesServiceStatus.OK) {
     return;
-  } else {
-    createMarkers(results, currentType).then(function() {
-      console.log('done making markers');
-    });
+  }
 
-    if (pagination.hasNextPage) {
-      pagination.nextPage();
-    }
-
-    clearMarkers(null);
-    clearMarkers(currentType);
+  for (var i = 0, result; result = results[i]; i++) {
+    if (i > 75) { break; }
+    createMarker(result);
   }
 }
 
 function createMarker(place, placeType) {
   var placeLoc = place.geometry.location;
 
-  var placeIcon = {
-    url: place.icon,
-    scaledSize: new google.maps.Size(25, 25),
-    origin: new google.maps.Point(0, 0),
-    anchor: new google.maps.Point(10, 0)
-  };
+  // var placeIcon = {
+  //   url: place.icon,
+  //   scaledSize: new google.maps.Size(25, 25),
+  //   origin: new google.maps.Point(0, 0),
+  //   anchor: new google.maps.Point(10, 0)
+  // };
 
   var marker = new google.maps.Marker({
     map: map,
-    icon: placeIcon,
+    // icon: placeIcon,
     position: place.geometry.location
   });
 
-  markersArray.push([currentType, marker]);
-
   google.maps.event.addListener(marker, 'click', function() {
-    infowindow.setContent(place.name);
-    infowindow.open(map, this);
-  });
-}
-
-function createMarkers(places, placeType) {
-  var deferred = $.Deferred();
-
-  deferred.resolve(function() {
-    for (var i = 0, place; place = places[i]; i++) {
-      createMarker(place, placeType);
-    }
-  });
-
-  return deferred.promise();
-}
-
-function clearMarkers(filterByType) {
-  $.each(markersArray, function(i, el) {
-    try {
-      if (el[0] === filterByType) {
-	el[1].setMap(map);
-      } else {
-	el[1].setMap(null);
-	// markersArray.splice(i, 1);
+    service.getDetails(place, function(result, status) {
+      if (status != google.maps.places.PlacesServiceStatus.OK) {
+        alert(status);
+        return;
       }
-    } catch(err) {
-      console.log('something broke: ', err);
-      markersArray.splice(i, 1);
-    }
+      infoWindow.setContent(result.name);
+      infoWindow.open(map, marker);
+    });
   });
 }
