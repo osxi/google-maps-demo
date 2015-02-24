fe01627141d8bc407141fd0711a23f2b8a296c30
osxi <osxi@users.noreply.github.com>
2015-02-24 08:58:43 -0600

    index on master: d955451 Factor out createMarker

--- places_client.js
+++ places_client.js
@@ -1,8 +1,15 @@
-var map;
-var infowindow;
+var map,
+    infowindow,
+    currentType,
+    markersArray = [];
 
-function initialize_map(el, lat, lon, locationMarkerText) {
+function initialize_map(el, lat, lon, locationMarkerText, showType) {
   var location = new google.maps.LatLng(lat, lon);
+
+  if (showType !== undefined) {
+    currentType = showType[0];
+  }
+  // console.log('currentType', currentType);
 
   map = new google.maps.Map(document.getElementById(el), {
     center: location,
@@ -22,14 +29,7 @@ function initialize_map(el, lat, lon, locationMarkerText) {
   var request = {
     location: location,
     radius: 10000,
-    types: [
-      'restaurant',
-      'bank',
-      'grocery_or_supermarket',
-      'museum',
-      'place_of_worship',
-      'airport'
-    ]
+    types: showType
   };
 
   infowindow = new google.maps.InfoWindow();
@@ -41,14 +41,20 @@ function callback(results, status, pagination) {
   if (status != google.maps.places.PlacesServiceStatus.OK) {
     return;
   } else {
-    createMarkers(results);
+    createMarkers(results, currentType).then(function() {
+      console.log('done making markers');
+    });
+
     if (pagination.hasNextPage) {
       pagination.nextPage();
     }
+
+    clearMarkers(null);
+    clearMarkers(currentType);
   }
 }
 
-function createMarker(place) {
+function createMarker(place, placeType) {
   var placeLoc = place.geometry.location;
 
   var placeIcon = {
@@ -64,14 +70,38 @@ function createMarker(place) {
     position: place.geometry.location
   });
 
+  markersArray.push([currentType, marker]);
+
   google.maps.event.addListener(marker, 'click', function() {
     infowindow.setContent(place.name);
     infowindow.open(map, this);
   });
 }
 
-function createMarkers(places) {
-  for (var i = 0, place; place = places[i]; i++) {
-    createMarker(place)
-  }
+function createMarkers(places, placeType) {
+  var deferred = $.Deferred();
+
+  deferred.resolve(function() {
+    for (var i = 0, place; place = places[i]; i++) {
+      createMarker(place, placeType);
+    }
+  });
+
+  return deferred.promise();
+}
+
+function clearMarkers(filterByType) {
+  $.each(markersArray, function(i, el) {
+    try {
+      if (el[0] === filterByType) {
+	el[1].setMap(map);
+      } else {
+	el[1].setMap(null);
+	// markersArray.splice(i, 1);
+      }
+    } catch(err) {
+      console.log('something broke: ', err);
+      markersArray.splice(i, 1);
+    }
+  });
 }
